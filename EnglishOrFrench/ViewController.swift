af1507e593bb052898b0358647b0d60c37035bd0
Chen Sihan <schen@lcs.on.ca>
2019-11-26 11:02:55 -0500

    check the characters one by one + storyboard changes

--- EnglishOrFrench/ViewController.swift
+++ EnglishOrFrench/ViewController.swift
@@ -9,12 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var inputText: UITextView!
+    
+    
+    @IBOutlet weak var analyzeResult: UILabel!
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
     }
-
-
+    
+    
+    
+    @IBAction func analyzeLanguage(_ sender: Any) {
+        
+        guard let inputTextAsString = inputText.text, inputTextAsString != "" else {
+            analyzeResult.text = "Please enter text to analyze."
+            return
+        }
+        var numberOfTs = 0
+        var numberOfSs = 0
+        for scalars in inputTextAsString.unicodeScalars {
+            let stringT = "tT"
+            let stringS = "sS"
+            
+            if stringT.contains(inputTextAsString) {
+                numberOfTs += 1
+            }
+            if stringS.contains(inputTextAsString) {
+                numberOfSs += 1
+            }
+            
+            
+            
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
